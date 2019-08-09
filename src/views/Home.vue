37e287e2786233ae20250b70bc86057668690591
Evgeniy Areshkin <evgeniy.areshkin@haymarket.in.ua>
2019-08-09 10:00:01 +0300

    Init project

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page_wrapper">
+    <Content msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Content from '@/components/Content.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Content
+  }
+}
+</script>
