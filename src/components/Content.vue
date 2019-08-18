5128b90c227d0990f1b3d2226ee1c0bdb27f727c
Julia Lopatka <julialopatka@MacBook-Pro-Julia.local>
2019-08-18 22:08:13 +0300

    TOBELEGAL-2 Slides 5-14

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -12,7 +12,7 @@ export default {
       options: {
         licenseKey: 'F1F7C056-F79543AF-9D4C42D2-E43F5FDE',
         menu: '#menu',
-        anchors: ['page1', 'page2', 'page3','page4'],
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9', 'page10', 'page11', 'page12', 'page13', 'page14'],
         // sectionsColor: ['#1A2730', '#1A2730', '#1A2730'],
 
         navigation: true,
